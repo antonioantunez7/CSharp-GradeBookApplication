137bcf1e35b4500f9710f64740c411918abb8ceb
antoniobarbosatereso <tereso.antunez@broxel.com>
2019-09-03 10:14:05 -0500

    a

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -3,7 +3,7 @@ using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
-    public class RankedGradeBook: BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name): base(name)
         {
